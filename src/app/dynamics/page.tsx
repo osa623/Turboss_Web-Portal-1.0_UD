884e9fa99e6276dbed62430f4528c67c128811fd
Osanda Muthukumarana <it22170798@my.sliit.lk>
2025-03-07 00:23:36 +0530

    tempPushing-hold

---
+++ src/app/dynamics/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+
+//images
+import heroBackground from '../assests/dynamics/subBackground.jpg';
+import sub1 from '../assests/subset/sub1.png';
+import sub2 from '../assests/subset/sub2.png';
+import sub3 from '../assests/subset/sub3.png';
+import sub4 from '../assests/subset/sub4.png';
+
+
+const subset =[
+
+    {id:1, image:sub1, title:'Torque'},
+    {id:2, image:sub2, title:'Displacement'},
+    {id:3, image:sub3, title:'Compression Ratio'},
+    {id:4, image:sub4, title:'Fuel Consumption'}
+]
+
+const Dynamics = () => {
+
+    const [hover, setHover] = useState<number | null>(null);
+
+  return (
+    <div className='relative h-screen w-full'>
+        <div className='absolute w-full min-h-screen bg-transparent'>
+
+            {/* Section */}
+            <div className='flex lgs:w-full h-auto items-center justify-center'>
+                <div className='flex lgs:w-[30vw] lgs:min-h-screen bg-secondary items-center justify-center'>
+
+                    <div className='flex flex-col lgs:w-[30vw] lgs:h-[40rem] items-center justify-center'>
+                        {
+                            subset.map((tool, index)=> (
+                                 <div key={index}  className='group relative flex w-auto h-auto cursor-pointer'
+                                 onMouseEnter={()=> setHover(index)}
+                                 onMouseLeave={()=> setHover(null)}
+                                 >
+                                       
+                                    <div className='relative h-[10rem] lgs:w-[30vw] bg-orange-400 overflow-hidden'>
+                                       <Image src={tool?.image} alt='turbo' className='flex object-cover group-hover:scale-125 w-full h-full transition-all border-2 duration-700 ease-in-out' layout='fill' />
+                                       <div className='absolute flex w-full h-1/4 items-center justify-center bg-gradient-to-b top-0 from-secondary to-transparent z-30'/>
+                                       <div className='absolute flex w-full h-3/4 items-center justify-center bg-gradient-to-t bottom-0 from-secondary to-transparent z-30'/>
+                                       <div className='absolute flex w-3/4 h-full items-center justify-center bg-gradient-to-r left-0 from-secondary to-transparent z-30'/>
+                                       <div className='absolute flex w-full h-full items-center justify-center left-0 z-30' style={{
+                                          boxShadow: hover == index ?
+                                           'inset 0px 0px 100px 1px rgba(255, 69, 0 , 0.9)' : 'inset 0px 0px 10px 1px rgba(0, 0, 0 , 0.9)',
+                                           transition:"box-shadow 0.5s ease-in-out",
+                                       }}/>
+ 
+  
+                                       <div className='absolute flex w-full h-full items-end justify-center bg-transparent z-40'>
+                                           <h2 className='font-poppins lgs:w-[8vw] lgs:h-[5rem] text-nowrap group-hover:scale-150 transition-all duration-700 ease-in-out  text-lg text-center text-primary'
+                                           style={{
+                                             fontWeight:'100'
+                                           }}>
+                                             {tool?.title}
+                                           </h2>
+ 
+                                       </div>  
+                                       <div className='absolute flex w-full h-full items-end justify-center bg-transparent z-30'>
+                                           <h2 className='font-poppins lgs:h-[5rem]  text-4xl text-center group-hover:scale-150 transition-all duration-700 ease-in-out opacity-15 text-primary'
+                                           style={{
+                                             fontWeight:'100'
+                                           }}>
+                                             {tool?.title}
+                                           </h2>
+ 
+                                       </div>  
+                                   </div> 
+                                 </div>                                  
+                            ))
+                        }
+
+                   </div>
+
+                </div>
+                <div className='relative flex lgs:w-[70vw] lgs:min-h-screen bg-yellow-700'>
+                    <Image src={heroBackground} layout='fill' objectFit='cover' objectPosition='center' alt='hero background' />
+                    <div className='absolute top-0 w-full h-full bg-gradient-to-t bottom-0 from-secondary to-transparent z-20'/>
+                    <div className='absolute top-0 w-full h-full bg-gradient-to-r left-0 from-secondary to-transparent z-20'/>
+                </div>
+
+
+            </div>
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Dynamics;
