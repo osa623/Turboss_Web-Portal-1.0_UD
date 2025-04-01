2e2a9144679081bf26f273459b0033b5a7eecea6
Osanda Muthukumarana <it22170798@my.sliit.lk>
2025-04-01 08:14:14 +0530

    temp --commit

--- src/app/labsection/[ id ]/page.tsx
+++ src/app/labsection/[ id ]/page.tsx
@@ -36,72 +36,10 @@ const Detailpage = () => {
 
     //hooks
 
-    const [offsetY, setOffsetY] = useState(0);
-    const [parallex1 , setParallex1] = useState(0);
+
     const [data, setData] = useState<labData | null>(null);
     const [loading, isLoading] = useState(true);
-    const [expandSection , setExpandSection] = useState(false);
-    const [click, setClick] = useState(false);     
-
   
-
-    //naviga
-    useEffect(() => {
-        const handleScroll = () => {
-            setOffsetY(window.scrollY * 0.05);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
-
-    useEffect(()=> {
-        const handleScroll = () => {
-            setParallex1(window.scrollY * 0.02);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
-
-
-
-
-
-  //handle expands 001
-  const handleExpand = () => {
-
-    setExpandSection(!expandSection);
-    
-  }
-
-
-  useEffect(() => {
-    if (!sessionStorage.getItem("refreshed")) {
-      sessionStorage.setItem("refreshed", "true");
-      window.location.reload();
-    }
-  }, []);
-
-  const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-      setIsClient(true);
-    }, []);
-
-
-    const [angle, setAngle] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAngle((prev) => prev + 30);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
 
   const router = useRouter();
 
@@ -173,19 +111,71 @@ useEffect(() => {
           <div className='relative flex w-full h-auto'>
                   
 
-                {/* Image Section */}  
-              <div className='flex h-[50rem] lgs:w-[50vw] justify-center items-center overflow-hidden bg-transparent'>
+                    {/* Image Section */} 
 
-                  <Image src={data?.image} alt={data?.name} width={1200} height={1200}  className='object-cover scale-150' />
-
-              </div>  
-
-
-                {/* Section Section */}  
-              <div className='flex h-[50rem] lgs:w-[50vw] justify-center items-center bg-transparent'>
+                  <div className='relative lgs:h-[50rem] lgs:w-[50vw] justify-center items-center bg-transparent'>
+                    
+                    <div className='relative flex lgs:w-[50vw] h-full bg-transparent z-20'>
+                      <Image src={data?.image || '/default-image.jpg'} alt={data?.name || 'Default Alt Text'} width={2400} height={2400}  className='object-cover' />
+                    </div> 
 
 
-              </div>  
+                              <div className='absolute top-0 left-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-r from-secondary to-transparent opacity-50'/>
+                              <div className='absolute top-0 left-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-b from-secondary to-transparent opacity-50'/>
+                              <div className='absolute top-0 left-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-t from-secondary to-transparent opacity-50'/>
+                              <div className='absolute top-0 right-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-l from-secondary to-transparent opacity-50'/>
+                              <div className='absolute top-0 left-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-r from-secondary to-transparent opacity-50'/>
+                              <div className='absolute top-0 left-0 lgs:w-[50vw] z-30 h-full bg-gradient-to-b from-secondary to-transparent opacity-50'/>
+
+
+
+                  <div className='absolute flex  top-0 left-0 lgs:w-[50vw] z-30 h-full items-center justify-center bg-transparent'>
+
+                  <div className="absolute top-0  items-start justify-start lgs:p-5">
+                                                
+                                                <div className="flex w-auto h-auto">
+
+                                                <h2 className="flex font-bricolagegrotesque text-primary lgs:text-2xl bg-orange-600 lgs:p-2">
+                                                Garage Section
+                                                </h2>
+                                                <span className="font-bricolagegrotesque text-orange-600 lgs:text-2xl bg-primary lgs:p-2">
+                                                    {data?.subname}
+                                                </span>
+
+                                                </div>
+
+                                                
+                  </div> 
+                    
+
+                   <h2 className='flex font-dmsans z-30 lgs:text-9xl text-orange-600'>
+                       {data?.name}
+                    </h2>
+
+                    <h2 className='absolute font-dmsans z-30 lgs:text-9xl text-orange-600'>
+                       {data?.name}
+                    </h2>
+                    
+                    <h2 className='absolute font-dmsans z-20 lgs:ml-3 lgs:text-9xl text-primary'>
+                       {data?.name}
+                    </h2>
+                    <h2 className='absolute z-20 font-dmsans lgs:text-nowrap  lgs:mt-16 text-gray-500 blur-sm opacity-30'style={{
+                      fontSize:'150px'
+                    }}>
+                       {data?.name}
+                    </h2>
+
+                  </div>
+
+                   </div>  
+
+
+                    {/* Section Section */}  
+
+                    <div className='flex h-[50rem] lgs:w-[50vw] justify-center items-center bg-transparent'>
+
+
+                    </div>  
 
 
 
